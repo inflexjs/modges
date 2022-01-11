2657475708bc95a1e31767df6113686487577b27
inflexjs <helidanger@yandex.ru>
2022-01-11 11:43:53 +0300

    mobile working[touches]

--- js/main.js
+++ js/main.js
@@ -2,58 +2,78 @@ const app = document.querySelector(".app"),
 	cursor = document.querySelector(".cursor"),
 	hp = document.querySelector(".hp"),
 	hearts = document.querySelectorAll(".heart"),
-	circle = document.querySelector(".circle");
+	circle = document.querySelector(".circle"),
+	mobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
 
-mouseX = 0, mouseY = 0, posX = 0, posY = 0
+mouseX = 0, mouseY = 0, posX = 0, posY = 0, touchX = 0, touchY = 0
 
 function mouseCoords(e) {
 	mouseX = e.pageX
 	mouseY = e.pageY
 }
 
-app.addEventListener("mousemove", e => {
-	mouseCoords(e);
-	cursor.classList.remove("hidden");
-	hp.classList.remove("hidden");
-})
+function touchCoords(e) {
+	touchX = e.touches[0].clientX;
+	touchY = e.touches[0].clientY;
+}
 
-app.addEventListener("touchmove", e => {
-	mouseCoords(e);
-	cursor.classList.remove("hidden");
-	hp.classList.remove("hidden");
-})
+if (mobile) {
 
-app.addEventListener("mouseout", e => {
-	cursor.classList.add("hidden");
-	hp.classList.add("hidden");
-})
+	app.addEventListener("touchmove", e => {
+		touchCoords(e);
+		cursor.classList.remove("hidden");
+		hp.classList.remove("hidden");
+	})
 
-app.addEventListener("click", e => {
-	console.log(e.target);
-	hp.removeChild(hp.lastElementChild);
-})
+	app.addEventListener("click", e => {
+		hp.removeChild(hp.lastElementChild);
+	})
 
-circle.addEventListener("mouseenter", e => {
-	console.log(mouseX)
-	console.dir(circle)
-	if (box.pageX == mouseX) {
-		console.log('Hi')
-	}
-})
+} else {
+
+	app.addEventListener("mousemove", e => {
+		mouseCoords(e);
+		cursor.classList.remove("hidden");
+		hp.classList.remove("hidden");
+	})
+
+	app.addEventListener("mouseout", e => {
+		cursor.classList.add("hidden");
+		hp.classList.add("hidden");
+	})
+
+	app.addEventListener("click", e => {
+		hp.removeChild(hp.lastElementChild);
+	})
+}
 
 gsap.to({}, .01, {
 	repeat: -1,
 
 	onRepeat: () => {
-		posX += (mouseX - posX) / 5
-		posY += (mouseY - posY) / 5
+		if (mobile) {
+			posX += (touchX - posX) / 5
+			posY += (touchY - posY) / 5
+		} else {
+			posX += (mouseX - posX) / 5
+			posY += (mouseY - posY) / 5
+		}
 
-		gsap.set(cursor, {
-			css: {
-				left: mouseX - 5,
-				top: mouseY - 5,
-			}
-		})
+		if (mobile) {
+			gsap.set(cursor, {
+				css: {
+					left: touchX - 5,
+					top: touchY - 5,
+				}
+			})
+		} else {
+			gsap.set(cursor, {
+				css: {
+					left: mouseX - 5,
+					top: mouseY - 5,
+				}
+			})
+		}
 
 		gsap.set(hp, {
 			css: {
